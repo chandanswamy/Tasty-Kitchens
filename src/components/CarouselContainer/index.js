99497373a222de0060f90f52161ebd580faaf3a2
chandanswamy <chandanswamy13214@gmail.com>
2023-07-14 09:21:40 +0000

    second Commit

---
+++ src/components/CarouselContainer/index.js
@@ -0,0 +1,117 @@
+import {Component} from 'react'
+import {withRouter} from 'react-router-dom'
+import Loader from 'react-loader-spinner'
+import Cookies from 'js-cookie'
+
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import './index.css'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
+class CarouselContainer extends Component {
+  state = {carouselData: [], apiStatus: apiStatusConstants.initial}
+
+  componentDidMount() {
+    this.getCarouselData()
+  }
+
+  getCarouselData = async () => {
+    this.setState({apiStatus: apiStatusConstants.inProgress})
+
+    const jwtToken = Cookies.get('jwt_token')
+
+    const apiUrl = 'https://apis.ccbp.in/restaurants-list/offers'
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(apiUrl, options)
+
+    if (response.ok === true) {
+      const fetchedData = await response.json()
+
+      const updatedData = fetchedData.offers.map(eachItem => ({
+        imageUrl: eachItem.image_url,
+        id: eachItem.id,
+      }))
+
+      this.setState({
+        carouselData: updatedData,
+        apiStatus: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({
+        apiStatus: apiStatusConstants.failure,
+      })
+    }
+  }
+
+  renderFailureView = () => {
+    const {history} = this.props
+    history.push('/not-found')
+  }
+
+  renderLoadingView = () => (
+    <div className="carousel-loader-container">
+      <Loader type="TailSpin" color="#F7931E" height="50" width="50" />
+    </div>
+  )
+
+  renderCarouselView = () => {
+    const {carouselData} = this.state
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+    }
+    return (
+      <div className="slider-container">
+        <Slider {...settings}>
+          {carouselData.map(eachItem => (
+            <div key={eachItem.id}>
+              <img
+                src={eachItem.imageUrl}
+                alt="offer"
+                className="carousel-image"
+              />
+            </div>
+          ))}
+        </Slider>
+      </div>
+    )
+  }
+
+  renderAllOffers = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiStatusConstants.success:
+        return this.renderCarouselView()
+      case apiStatusConstants.failure:
+        return this.renderFailureView()
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingView()
+      default:
+        return null
+    }
+  }
+
+  render() {
+    return <div className="carousel">{this.renderAllOffers()}</div>
+  }
+}
+
+export default withRouter(CarouselContainer)
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,160 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import Loader from 'react-loader-spinner'
 import Header from '../Header'
 import Footer from '../Footer'
+import CarouselContainer from '../CarouselContainer'
+import RestaurantHeader from '../RestaurantHeader'
 
-const Home = () => {
-  const tabActive = 'home-active'
-  return (
-    <>
-      <Header tabActive={tabActive} />
-      <div>Home</div>
-      <Footer />
-    </>
+import './index.css'
+import RestaurantCard from '../RestaurantCard'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
+const sortByOptions = [
+  {
+    id: 0,
+    displayText: 'Highest',
+    value: 'Highest',
+  },
+  {
+    id: 2,
+    displayText: 'Lowest',
+    value: 'Lowest',
+  },
+]
+
+class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      restaurantList: [],
+      apiStatus: apiStatusConstants.initial,
+      activeOptionId: sortByOptions[1].value,
+      searchInput: '',
+      activePage: 1,
+      totalItems: 0,
+    }
+  }
+
+  componentDidMount() {
+    this.getRestaurant()
+  }
+
+  getRestaurant = async () => {
+    this.setState({
+      apiStatus: apiStatusConstants.inProgress,
+    })
+    const jwtToken = Cookies.get('jwt_token')
+    const {activeOptionId, searchInput, activePage} = this.state
+    const limit = 9
+    const offset = (activePage - 1) * limit
+
+    const apiUrl = `https://apis.ccbp.in/restaurants-list?search=${searchInput}&offset=${offset}&limit=${limit}&sort_by_rating=${activeOptionId}`
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(apiUrl, options)
+
+    if (response.ok === true) {
+      const fetchedData = await response.json()
+      const updatedData = fetchedData.restaurants.map(eachItem => ({
+        hasOnlineDelivery: eachItem.has_online_delivery,
+        userRating: eachItem.user_rating,
+        name: eachItem.name,
+        hasTableBooking: eachItem.has_table_booking,
+        isDeliveringNow: eachItem.is_delivering_now,
+        costForTwo: eachItem.cost_for_two,
+        cuisine: eachItem.cuisine,
+        imageUrl: eachItem.image_url,
+        id: eachItem.id,
+        menuType: eachItem.menu_type,
+        location: eachItem.location,
+        opensAt: eachItem.opens_at,
+        groupByTime: eachItem.group_by_time,
+      }))
+
+      const totalItems = fetchedData.total
+
+      this.setState({
+        restaurantList: updatedData,
+        apiStatus: apiStatusConstants.success,
+        totalItems,
+      })
+    } else {
+      this.setState({
+        apiStatus: apiStatusConstants.failure,
+      })
+    }
+  }
+
+  renderLoadingView = () => (
+    <div className="restaurant-loader-container">
+      <Loader type="TailSpin" color="#F7931E" height="50" width="50" />
+    </div>
   )
+
+  renderFailureView = () => {
+    const {history} = this.props
+    history.push('/not-found')
+  }
+
+  renderRestaurantListView = () => {
+    const {restaurantList, activeOptionId, activePage, totalItems} = this.state
+    return (
+      <div className="all-restaurant-container">
+        <RestaurantHeader
+          activeOptionId={activeOptionId}
+          sortByOptions={sortByOptions}
+          changeSortby={this.changeSortby}
+        />
+        <ul className="restaurant-list">
+          {restaurantList.map(restaurant => (
+            <RestaurantCard
+              restaurantData={restaurant}
+              key={restaurant.id}
+              testid="restaurant-item"
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  renderAllRestaurants = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiStatusConstants.success:
+        return this.renderRestaurantListView()
+      case apiStatusConstants.failure:
+        return this.renderFailureView()
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingView()
+      default:
+        return null
+    }
+  }
+
+  render() {
+    const tabActive = 'home-active'
+    return (
+      <div className="home-route">
+        <Header tabActive={tabActive} />
+        <CarouselContainer />
+        <div className="home-restaurants">{this.renderAllRestaurants()}</div>
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default Home
